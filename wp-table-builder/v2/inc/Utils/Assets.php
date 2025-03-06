323049d4b03344f45749ae121b19f9c3024e4331
Sean Murphy <sean@iamseanmurphy.com>
2025-03-06 18:18:21 +0000

    2.0.2

--- wp-table-builder/v2/inc/Utils/Assets.php
+++ wp-table-builder/v2/inc/Utils/Assets.php
@@ -54,6 +54,7 @@ class Assets
             'SECURITY_CODE' => wp_create_nonce('wptb-import-security-nonce'),
             'EXPORT_KEY' => wp_create_nonce('wptb_table_export_main_export'),
             'SETTINGS' => [
+                'is_authorized'=> current_user_can('manage_options'),
                 'version' => WPTableBuilder::VERSION,
                 'general' => get_option('wp_table_builder_settings'),
                 'table_style' => get_option('wptb-general-styles'),
@@ -69,6 +70,7 @@ class Assets
             'NONCE' => [
                 'table' => wp_create_nonce('wptb_nonce_table'),
                 'preview' => wp_create_nonce('wptb_nonce_table_preview'),
+                'create_term' => wp_create_nonce('wptb_create_term'),
             ]
 
         ];
