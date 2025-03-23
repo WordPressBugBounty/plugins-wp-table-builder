75720b2c15195fb74b83566fd30149dcde5f59a9
Sean Murphy <sean@iamseanmurphy.com>
2025-03-23 18:18:18 +0000

    2.0.4

--- wp-table-builder/v2/build/index.asset.php
+++ wp-table-builder/v2/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '2efd978515546f83aab4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '51253280a4f0f654f899');
